2a3a80615e5607830d86b15d7b2c87602476c518
Rick Yiu <rickyiu@google.com>
2020-06-15 22:03:08 +0800

    ANDROID: sched: consider stune boost margin when computing energy
    If CONFIG_SCHED_TUNE is enabled, it does not use boosted cpu util
    to compute energy, so it could not reflect the real freq when a
    cpu has boosted tasks on it. Addressing it by adding boost margin
    if type is FREQUENCY_UTIL in schedutil_cpu_util().
    
    Bug: 158637636
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I570920cb1e67d07de87006fca058d50e9358b7cd
    Signed-off-by: 1tovgar <tovgar@proton.me>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -178,8 +178,11 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	sg_policy->cached_raw_freq = freq;
 	return cpufreq_driver_resolve_freq(policy, freq);
 }
+#endif
+
 extern long
 schedtune_cpu_margin_with(unsigned long util, int cpu, struct task_struct *p);
+
 /*
  * This function computes an effective utilization for the given CPU, to be
  * used for frequency selection given the linear relation: f = u * f_max.
--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -12,15 +12,13 @@ struct target_nrg {
 	struct reciprocal_value rdiv;
 };
 
-int schedtune_cpu_boost(int cpu);
+int schedtune_cpu_boost_with(int cpu, struct task_struct *p);
 int schedtune_task_boost(struct task_struct *tsk);
 
 int schedtune_prefer_idle(struct task_struct *tsk);
 
 void schedtune_enqueue_task(struct task_struct *p, int cpu);
 void schedtune_dequeue_task(struct task_struct *p, int cpu);
-
-unsigned long stune_util(int cpu, unsigned long other_util);
 
 #ifdef CONFIG_UCLAMP_TASK_GROUP
 extern struct mutex uclamp_mutex;
@@ -35,7 +33,7 @@ uclamp_task_update_active(struct task_struct *p, unsigned int clamp_id);
 #endif
 #else /* CONFIG_SCHED_TUNE */
 
-#define schedtune_cpu_boost(cpu)  0
+#define schedtune_cpu_boost_with(cpu, p)  0
 #define schedtune_task_boost(tsk) 0
 
 #define schedtune_prefer_idle(tsk) 0
