ddc203e7549d676adbd73b9e532ce9c53220d1af
1tovgar <tovgar@proton.me>
2026-06-08 16:46:18 +0500

    Revert "[ALPS04362512] ufs-mtk: Provide clk-gating event history"
    This reverts commit bf0bb16c258d22a18aa359b63a38f560f94284e5.
    
    Signed-off-by: 1tovgar <tovgar@proton.me>

--- drivers/scsi/ufs/ufs-mediatek-dbg.h
+++ drivers/scsi/ufs/ufs-mediatek-dbg.h
@@ -77,7 +77,6 @@ enum cmd_hist_event {
 	CMD_PERF_MODE,
 	CMD_DEBUG_PROC,
 	CMD_GENERIC,
-	CMD_CLK_GATING,
 };
 
 struct utp_cmd_struct {
@@ -99,10 +98,6 @@ struct uic_cmd_struct {
 	int result;
 };
 
-struct clk_gating_event_struct {
-	u8 state;
-};
-
 struct cmd_hist_struct {
 	u8 cpu;
 	enum cmd_hist_event event;
@@ -112,7 +107,6 @@ struct cmd_hist_struct {
 	union {
 		struct utp_cmd_struct utp;
 		struct uic_cmd_struct uic;
-		struct clk_gating_event_struct clk_gating;
 	} cmd;
 };
 
